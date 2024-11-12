63406241af47bda002723134eadc2b0dfbbe0c8d
hridayanp <hridayanphukan123@gmail.com>
2024-11-12 23:00:14 +0530

    added authentication from scratch

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
+import { AuthService } from './auth.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
   imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
